86efbdba8b93310ca1fafae3e5db9c2fa3103b35
amnhh <2570079254@qq.com>
2017-03-21 10:46:38 +0800

    modified jsonp files

--- jsonp/index.php
+++ jsonp/index.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Author : anning
- * Date :
- * Description :
- */
 $data = '[{"id" : "1", "name" : "Amnhh"}, {"id" : "2", "name" : "FFM"}]';
 $data = "JSON_CALLBACK(" . $data . ")";
 echo $data;
